331b571ccdf5d1f94b2b260f2a2c3bafc876f32b
Lucian Bumb <bumblucian@yahoo.com>
2025-12-09 19:18:29 +0200

    Remove legacy root copy; keep MarkdownViewerElectron only

--- generate-icon.js
+++
@@ -1,9 +0,0 @@
-const { default: pngToIco } = require('png-to-ico');
-const fs = require('fs');
-
-pngToIco('markdownviewer.png')
-  .then(buf => {
-    fs.writeFileSync('markdownviewer.ico', buf);
-    console.log('✓ Icon converted successfully!');
-  })
-  .catch(console.error);
